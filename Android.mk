0782b8cdeaa48743edf564185eb17c06b9f1d167
bonuzzz <bonuzzz@gmail.com>
2018-02-09 19:56:45 +0300

    init

---
+++ Android.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter treltexx trhpltexx, $(TARGET_DEVICE)),)
+
+$(shell mkdir -p out/target/product/$(TARGET_DEVICE)/obj/KERNEL_OBJ/drivers/net/wireless/bcmdhd4358)
+$(shell mkdir -p out/target/product/$(TARGET_DEVICE)/obj/KERNEL_OBJ/drivers/sensorhub/brcm/factory)
+$(shell mkdir -p out/target/product/$(TARGET_DEVICE)/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor)
+$(shell mkdir -p out/target/product/$(TARGET_DEVICE)/obj/KERNEL_OBJ/security/tima_uevent)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
---
+++ BoardConfigCommon.mk
@@ -0,0 +1,207 @@
+LOCAL_PATH := device/samsung/trelte-common
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_SLSI_VARIANT := cm
+TARGET_SOC := exynos5433
+
+# CPU
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a53.a57
+
+# big.LITTLE load balancing
+# ENABLE_CPUSETS := true
+
+TARGET_BOOTLOADER_BOARD_NAME := universal5433
+
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+#BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
+
+# /proc/partitions * 2 (why?) * BLOCK_SIZE (512) = size in bytes
+BOARD_BOOTIMAGE_PARTITION_SIZE := 14680064
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3774873600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27028094976
+# blockdev --getbsz /dev/block/mmcblk0p9
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# ValidityService
+BOARD_USES_VALIDITY := true
+
+# PowerHAL
+TARGET_POWERHAL_VARIANT := samsung
+
+# Sensors
+TARGET_NO_SENSOR_PERMISSION_CHECK := true
+
+# Kernel
+#TARGET_KERNEL_SOURCE := kernel/samsung/trelte
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos5433
+
+# Use these flags if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Extended filesystem support
+TARGET_KERNEL_HAVE_EXFAT := true
+TARGET_KERNEL_HAVE_NTFS := true
+
+# F2FS support
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Camera
+BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
+TARGET_HAS_LEGACY_CAMERA_HAL1 := true
+
+# Graphics
+USE_OPENGL_RENDERER := true
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+#MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+#MAX_EGL_CACHE_SIZE := 2048*1024
+
+# frameworks/native/services/surfaceflinger
+# Android keeps 2 surface buffers at all time in case the hwcomposer
+# misses the time to swap buffers (in cases where it takes 16ms or
+# less). Use 3 to avoid timing issues.
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# Mixer
+#BOARD_USE_BGRA_8888 := true
+
+# HWCServices
+BOARD_USES_HWC_SERVICES := true
+
+# Virtual Display
+BOARD_USES_VIRTUAL_DISPLAY := true
+
+# HDMI
+BOARD_USES_NEW_HDMI := true
+BOARD_USES_GSC_VIDEO := true
+BOARD_USES_CEC := true
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+
+BOARD_USES_NEON_BLITANTIH := true
+BOARD_USES_FIMGAPI_V4L2 := false
+
+# (G)SCALER
+BOARD_USES_SCALER := true
+#BOARD_USES_DT := true
+
+# Samsung LSI OpenMAX
+TARGET_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED
+
+# Audio HAL variant
+#TARGET_AUDIOHAL_VARIANT := samsung
+
+# Samsung Seiren audio
+BOARD_USE_ALP_AUDIO := true
+BOARD_USE_SEIREN_AUDIO := true
+
+# Samsung OpenMAX Video
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USE_DMA_BUF := true
+BOARD_USE_ANB_OUTBUF_SHARE := true
+BOARD_USE_IMPROVED_BUFFER := true
+BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_CSC_HW := false
+BOARD_USE_QOS_CTRL := false
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_VP8ENC_SUPPORT := true
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Hardware
+#BOARD_HARDWARE_CLASS += device/samsung/trelte-common/cmhw
+#BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
+BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+
+# WIFI
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE                := bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
+WIFI_DRIVER_NVRAM_PATH           := "/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_FW_PATH_STA          := "/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/etc/wifi/bcmdhd_apsta.bin"
+
+# Webkit
+#ENABLE_WEBGL := true
+
+# WFD
+BOARD_USES_WFD := true
+
+# Wifi Macloader
+BOARD_HAVE_SAMSUNG_WIFI := true
+
+# Charger/Healthd
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+RED_LED_PATH := "/sys/class/leds/led_r/brightness"
+GREEN_LED_PATH := "/sys/class/leds/led_g/brightness"
+BLUE_LED_PATH := "/sys/class/leds/led_b/brightness"
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
+
+# RIL
+BOARD_VENDOR := samsung
+BOARD_PROVIDES_LIBRIL := true
+#BOARD_MODEM_NEEDS_VIDEO_CALL_FIELD := true
+TARGET_IGNORE_RO_BOOT_REVISION := true
+
+# RIL.java overwrite
+BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.universal5433
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+	device/samsung/trelte-common/sepolicy
+
+# Video scaling issue workaround
+TARGET_OMX_LEGACY_RESCALING := true
+
+# Seccomp filters
+BOARD_SECCOMP_POLICY := device/samsung/trelte-common/seccomp
+
+# Bootanimation Dimensions
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+#TARGET_PROVIDES_LIBLIGHT := false
