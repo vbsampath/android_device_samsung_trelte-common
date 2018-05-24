0c15589151a5514e8c30c44878a354beee755fc9
DL <dl12345@github.com>
2018-05-24 15:08:14 +0700

    Use BGRA byte order
    Change-Id: I6d77649d9bcc0ee2727be04cf3b4ca0fde98f754

--- board/graphics.mk
+++ board/graphics.mk
@@ -4,3 +4,8 @@ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
 # Virtual Display
 BOARD_USES_VIRTUAL_DISPLAY := true
+
+# Using the BGRA byteorder over the standard RGBA one
+# apparently yields a performance increase of approx.
+# 5% on some GPU hardware (including Mali)
+BOARD_USE_BGRA_8888 := true
