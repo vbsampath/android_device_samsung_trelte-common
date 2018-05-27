66a388c2c87e7c4d9bd2e11d610b715ec24935d1
DL <dl12345@github.com>
2018-05-27 20:16:08 +0700

    Add Google master apns-conf.xml
    Fix broken/incomplete LineageOS version
    
    Change-Id: I8bd819953af010348e04b92112adee8363e186d4

---
+++ copy/telephony.mk
@@ -0,0 +1,3 @@
+# APN Definitions - override incomplete/broken lineageos version with Google master version
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/telephony/apns-conf.xml:system/etc/apns-conf.xml
